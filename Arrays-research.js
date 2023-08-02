c7f7f192fe2bc1561279cbd6650641884f3f1f26
Christian <christianbur1248@gmail.com>
2023-08-02 15:46:03 -0500

    1

---
+++ Arrays-research.js
@@ -0,0 +1,13 @@
+
+
+// at
+
+const arrayAt = [1,2,3,4,5];
+const arrayIndex = `${arrayAt.at(2)}` 
+console.log(arrayIndex)
+
+// concat
+
+const arrayConcat = [1,2,3,4,5];
+const array2 = [5,6,7,8]
+const array3 = 
